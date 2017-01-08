6ce33f9ec7e646dcef7fe4cde2699a0554d6eb12
Rajkumar Subramanian <srajkumarms@gmail.com>
2017-01-07 23:13:07 -0500

    gruntfile generation and pom.xml update

--- src/main/Gruntfile.js
+++ src/main/Gruntfile.js
@@ -1,28 +1,16 @@
 module.exports = function (grunt) {
     "use strict";
 
-    grunt.initConfig({
-        
-    });
+    require("load-grunt-tasks")(grunt);
+    // grunt.initConfig({
+    //
+    // });
 
-    // A very basic default task.
-    grunt.registerTask('default', 'Log some stuff.', function defaultProfile() {
-        grunt.log.write('Logging some stuff...').ok();
-    });
-
-    grunt.registerTask('dev', 'Developer-Profile', function developerProfile() {
-        grunt.log.write("Dev Profile... ").ok();
-    });
-
-    grunt.registerTask('qc', 'QC-Profile', function qcProfile() {
-        grunt.log.write('QC Profile... ').error();
-    });
-
-    grunt.registerTask('uat', 'UAT-Profile', function qcProfile() {
-        grunt.log.write('UAT Profile... ').ok();
-    });
-
-    grunt.registerTask('prod', 'PROD-Profile', function qcProfile() {
-        grunt.log.write('PROD Profile... ').ok();
+    grunt.registerTask('build', 'Developer-Profile', function developerProfile(target) {
+        if (target === "minifed") {
+            grunt.log.write("Building minifed version... ").ok();
+        } else {
+            grunt.log.write("Building non-minifed version... ").ok();
+        }
     });
 };
